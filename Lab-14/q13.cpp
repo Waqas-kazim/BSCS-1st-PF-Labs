429ae809c8ca2505a563a67ede5fe96d370b0c27
waqas-kazim <ranawaqasofficial864@gmail.com>
2026-01-03 14:29:00 +0500

    .

---
+++ Lab-14/q13.cpp
@@ -0,0 +1,30 @@
+// 13.	Write a program to read integers from a file and count how many are even and how many are odd.
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream inputFile("q13.txt");
+    if (!inputFile) {
+        cerr << "Unable to open file q13.txt"<<endl;
+        return 1; // Exit with error code
+    }
+
+    int number;
+    int evenCount = 0;
+    int oddCount = 0;
+
+    while (inputFile >> number) {
+        if (number % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    inputFile.close();
+
+    cout << "Even numbers count: " << evenCount << endl;
+    cout << "Odd numbers count: " << oddCount << endl;
+
+    return 0;
+}
